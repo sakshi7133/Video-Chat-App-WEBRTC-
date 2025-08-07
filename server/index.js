ed060073f767c4a5e74fbbdaba1d19e243e7178f
sakshi7133 <sakshisinghaug2003@gmail.com>
2025-08-08 00:07:18 +0530

    Add server-side setup with Express and Socket.io for signaling

---
+++ server/index.js
@@ -0,0 +1,44 @@
+const express= require("express")
+const bodyParser=require("body-parser");
+const {Server}=require("socket.io");
+
+const io= new Server(
+    {
+        cors: true,
+    }
+);
+const app= express();
+
+app.use(bodyParser.json());
+
+const emailToSocketMapping= new Map();
+const socketToEmailMapping=new Map();
+
+io.on('connection',socket =>{
+    console.log("new connection");
+    socket.on("join-room",(data)=>{
+        const{emailId,roomId} =data;
+        console.log("user",emailId,"Joined Room",roomId);
+       
+        emailToSocketMapping.set(emailId,socket.id);
+        socketToEmailMapping.set(socket.id,emailId);
+        socket.join(roomId);
+        socket.emit("joined-room",{roomId});
+        socket.broadcast.to(roomId).emit("user-joined",{emailId});
+         });
+    socket.on("call-user",(data)=>{
+        const{emailId,offer}=data;
+        const fromEmail=socketToEmailMapping.get(socket.id);
+        const socketId=emailToSocketMapping.get(emailId);
+        socket.to(socketId).emit('incoming-call',{from :fromEmail,offer})
+    });   
+    socket.on("call-accepted",(data)=>{
+        const{emailId,ans}=data;
+        const socketId=emailToSocketMapping.get(emailId);
+        socket.to(socketId).emit("call-accepted",{ans});
+
+    });
+});
+
+app.listen(8000, ()=> console.log("http server running at port 8000"));
+io.listen(8001);
